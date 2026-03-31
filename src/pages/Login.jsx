715a10a25fea513f856351269daa9c32c6f04d58
Mohit Malik <malikmohit332@gmail.com>
2026-03-31 21:21:18 +0530

    Designed Login Page UI with Tailwind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600">
+      <div className="bg-white p-8 rounded-2xl shadow-xl w-96">
+        <h2 className="text-3xl font-bold text-center mb-6 text-gray-800">
+          Job Portal Login
+        </h2>
+
+        <p className="text-center text-gray-500 mb-6">
+          Welcome back! Please login to your account.
+        </p>
+
+        <input 
+          type="email"
+          placeholder="Enter Your Email"
+          className="w-full mb-4 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input 
+          type="password"
+          placeholder="Enter your password"
+          className="w-full mb-4 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} 
+        />
+
+        <button className="w-full bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-300">
+          Login
+        </button>
+
+        <p className="text-center text-gray-500 mt-4 text-sm">
+          Don’t have an account? <span className="text-blue-500 cursor-pointer">Register</span>
+        </p>
+      </div>
     </div>
   )
 }
