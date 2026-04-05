0c5bc3947d245012a14cbdfa17bdbba5d5fa04de
Mohit Malik <malikmohit332@gmail.com>
2026-04-05 15:05:41 +0530

    Created Recruiter page to post the JOBS

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Jobs from './pages/Jobs'
+import Recruiter from './pages/Recruiter'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='jobs' element={<Jobs />}/>
+        <Route path='/recruiter' element={<Recruiter />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Recruiter.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import API from "../services/api";
+
+
+function Recruiter() {
+  const [job, setJob] = useState({
+    title: "",
+    description: "",
+    company: "",
+    location: "",
+    salary: ""
+  })
+
+  const handleChange = (e) => {
+    setJob({...job, [e.target.name]: e.target.value });
+  }
+
+  const handleCreateJob = async () => {
+    try{
+      await API.post("/jobs/create", job);
+      alert("Job Created...");
+
+    } catch(err){
+      console.error(err);
+      alert("Error Creating Job...");
+    }
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      
+      <div className="bg-white p-8 rounded-xl shadow-md w-96">
+        <h2 className="text-2xl font-bold mb-4 text-center">
+          Post a Job
+        </h2>
+
+        <input name="title" placeholder="Title" onChange={handleChange} className="w-full mb-3 p-2 border rounded"/>
+        <input name="company" placeholder="Company" onChange={handleChange} className="w-full mb-3 p-2 border rounded"/>
+        <input name="location" placeholder="Location" onChange={handleChange} className="w-full mb-3 p-2 border rounded"/>
+        <input name="salary" placeholder="Salary" onChange={handleChange} className="w-full mb-3 p-2 border rounded"/>
+        <textarea name="description" placeholder="Description" onChange={handleChange} className="w-full mb-3 p-2 border rounded"/>
+
+        <button
+          onClick={handleCreateJob}
+          className="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600"
+        >
+          Create Job
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Recruiter;
